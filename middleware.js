3049935f8ffac3604b0eb1f18dca1159f49047d6
lin0ks2 <132504799+lin0ks2@users.noreply.github.com>
2025-12-03 23:24:26 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,25 @@
 import { NextResponse } from "next/server";
 
+const USER = "lin0ks";
+const PASS = "sk0nil";
+
 export function middleware(req) {
   const auth = req.headers.get("authorization");
-  if (auth !== `Basic ${btoa("user:pass")}`) {
-    return new NextResponse("Auth Required", {
+
+  const expected = "Basic " + Buffer.from(`${USER}:${PASS}`).toString("base64");
+
+  if (auth !== expected) {
+    return new NextResponse("Authentication required", {
       status: 401,
-      headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
     });
   }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: "/:path*" // защищаем всё
+};
